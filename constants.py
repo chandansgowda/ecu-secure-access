e8cf1ab3db1c551d71a6dbdc787e08678383e0cf
chandansgowda <chandansuresh007@gmail.com>
2023-06-21 21:33:30 +0530

    added comments

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # This file holds the host address and port numbers for the ECU and Trust Center Server
 
-ECU_HOST = 'localhost'
+ECU_HOST = 'localhost' # Set this to Computer A's IPv4 Address
 ECU_PORT = 9002
 
-TRUST_CENTER_HOST = 'localhost'
+TRUST_CENTER_HOST = 'localhost' # Set this to Computer A's IPv4 Address
 TRUST_CENTER_PORT = 8001
